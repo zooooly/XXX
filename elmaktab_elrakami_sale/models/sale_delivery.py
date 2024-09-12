98153d5b94f55b6427687da23194953347d04e5b
Malaz-svg <malazmohammed29@gmail.com>
2024-09-12 14:43:15 +0200

    Change Name

--- elmaktab_elrakami_sale/models/sale_delivery.py
+++
@@ -1,8 +0,0 @@
-from odoo import fields, models, api
-
-
-class SaleDelivery(models.Model):
-    _name = 'sale.delivery'
-    _description = 'Sale Delivery'
-
-    name = fields.Char()
--- elmaktab_elrakami_sale/models/sale_type.py
+++
@@ -1,8 +0,0 @@
-from odoo import fields, models, api
-
-
-class SaleType(models.Model):
-    _name = 'sale.type'
-    _description = 'Sale Type'
-
-    name = fields.Char()
