efe691c2e08624ef154e425c9a977f0fafbc599d
Malaz-svg <malazmohammed29@gmail.com>
2024-09-16 22:14:25 +0200

    Update

--- zouljanaheen_account/models/account_move.py
+++ zouljanaheen_account/models/account_move.py
@@ -5,4 +5,16 @@ class AccountInvoiceReport(models.Model):
     _inherit = 'account.invoice.report'
 
 
-    sale_person_id  = fields.Many2one('hr.employee' related ="partner_id.x_studio_sale_person")
+    sale_person_id  = fields.Many2one('hr.employee')
+        # related ="partner_id.x_studio_sale_person")
+
+
+
+
+
+class res_partner(models.Model):
+    _inherit = 'res.partner'
+
+    date = fields.Date()
+    name = fields.Char()
+    vat = fields.Char()
